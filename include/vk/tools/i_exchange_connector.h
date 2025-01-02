b18e9985be90b08e56adf1ff480a329b81e95fe7
Vitezslav Kot <vitezslav.kot@stonky.cz>
2025-01-02 22:43:35 +0100

    Refactor ExchangeConnector

--- include/vk/tools/i_exchange_connector.h
+++ include/vk/tools/i_exchange_connector.h
@@ -12,29 +12,33 @@ Copyright (c) 2022 Vitezslav Kot <vitezslav.kot@gmail.com>.
 #include <string>
 #include <boost/config.hpp>
 #include <boost/dll/alias.hpp>
+#include <nlohmann/json_fwd.hpp>
 #include <vk/tools/log_utils.h>
-#include <enum.h>
 
-BETTER_ENUM(ExchangeId, std::int32_t,
-            BinanceFutures,
-            Bybit,
-            MEXC,
-            OKX
-)
+enum class ExchangeId : std::int32_t {
+    BinanceFutures,
+    BinanceSpot,
+    BybitFutures,
+    BybitSpot,
+    MEXCFutures,
+    MEXCSpot,
+    OKXFutures,
+    OKXSpot
+};
 
-enum OrderSide {
+enum class OrderSide : std::int32_t {
     Sell = 0,
     Buy = 1
 };
 
-enum OrderType {
+enum class OrderType : std::int32_t {
     Market = 0,
     Limit = 1,
     Stop = 2,
     StopLimit = 3
 };
 
-enum OrderStatus {
+enum class OrderStatus : std::int32_t {
     New = 0,
     PartiallyFilled = 1,
     Filled = 2,
@@ -44,7 +48,7 @@ enum OrderStatus {
     Expired = 6
 };
 
-enum TimeInForce {
+enum class TimeInForce : std::int32_t {
     GTC = 0,
     IOC = 1,
     FOK = 2
@@ -52,49 +56,67 @@ enum TimeInForce {
 
 struct Order {
     /// Order quantity
-    double quantity = 0.0;
+    double quantity{};
 
     /// Symbol name - e.g. BTCUSDT
-    std::string asset;
+    std::string symbol{};
 
     /// Order side - e.g. OrderSide::Buy
-    OrderSide side = Buy;
+    OrderSide side{OrderSide::Buy};
 
     /// Order type - e.g. OrderType::Limit
-    OrderType type = Limit;
+    OrderType type{OrderType::Limit};
 
     /// Time in force - e.g. TimeInForce::GTC
-    TimeInForce timeInForce = GTC;
+    TimeInForce timeInForce{TimeInForce::GTC};
 
     /// Either limit or stop price
-    double price = 0.0;
+    double price{};
 
     /// Client order id
-    std::string clientOrderId;
+    std::string clientOrderId{};
+
+    std::unique_ptr<nlohmann::json> customData{};
 };
 
 struct Trade {
     /// Average realized price
-    double averagePrice = 0.0;
+    double averagePrice{};
 
     /// Realized quantity
-    double filledQuantity = 0.0;
+    double filledQuantity{};
 
     /// Fill timestamp in ms since Epoch
-    std::int64_t fillTime = 4;
+    std::int64_t fillTime{4};
 
     /// Order status - e.g. OrderStatus::New
-    OrderStatus orderStatus = New;
+    OrderStatus orderStatus{OrderStatus::New};
+
+    std::unique_ptr<nlohmann::json> customData{};
 };
 
-struct Price {
-    double bidPrice = 0.0;
+struct TickerPrice {
+    double bidPrice{};
+    double askPrice{};
+    double bidQty{};
+    double askQty{};
+    std::int64_t time{};
 
-    double askPrice = 0.0;
+    std::unique_ptr<nlohmann::json> customData{};
 };
 
 struct Balance {
-    double balance = 0.0;
+    double balance{};
+
+    std::unique_ptr<nlohmann::json> customData{};
+};
+
+struct FundingRate {
+    std::string m_symbol{};
+    double m_fundingRate{};
+    std::int64_t m_fundingTime{};
+
+    std::unique_ptr<nlohmann::json> customData{};
 };
 
 struct BOOST_SYMBOL_VISIBLE IExchangeConnector {
@@ -104,19 +126,24 @@ struct BOOST_SYMBOL_VISIBLE IExchangeConnector {
 
     [[nodiscard]] virtual std::string name() const = 0;
 
-    virtual void setLoggerCallback(const onLogMessage& onLogMessageCB) = 0;
+    virtual void setLoggerCallback(const onLogMessage &onLogMessageCB) = 0;
 
-    virtual void login(const std::tuple<std::string, std::string, std::string>& credentials) = 0;
+    virtual void login(const std::tuple<std::string, std::string, std::string> &credentials) = 0;
 
     virtual void setDemo(bool demo) = 0;
 
     [[nodiscard]] virtual bool isDemo() const = 0;
 
-    virtual Trade trade(const Order& order) = 0;
+    virtual Trade placeOrder(const Order &order) = 0;
 
-    [[nodiscard]] virtual Price getAssetPrice(const std::string& asset) const = 0;
+    [[nodiscard]] virtual TickerPrice getSymbolTickerPrice(const std::string &symbol) const = 0;
 
-    [[nodiscard]] virtual Balance getAccountBalance(const std::string& currency) const = 0;
+    [[nodiscard]] virtual Balance getAccountBalance(const std::string &currency) const = 0;
+
+    [[nodiscard]] FundingRate getLastFundingRate(const std::string &symbol) const;
+
+    [[nodiscard]] virtual std::vector<FundingRate> getFundingRates(const std::string &symbol, std::int64_t startTime,
+                                                                   std::int64_t endTime) const = 0;
 };
 
 #endif // INCLUDE_VK_TOOLS_I_EXCHANGE_CONNECTOR_H
